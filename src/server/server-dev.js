219ad2669da24c30e0322ffff18621e7eafeef81
Ankan Sircar <ankan.sircar@gmail.com>
2019-12-18 16:41:48 +1100

    Refactored and added webpack

---
+++ src/server/server-dev.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+import express from 'express';
+const bodyparser = require('body-parser');
+const config = require('config');
+const heartbeatRouter = require('../routes/heartbeat');
+const sendemailRouter = require('../routes/sendemail');
+
+
+const app = express();
+const port = config.get('port');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use('/api/v1/', heartbeatRouter);
+
+app.use('/api/v1/', sendemailRouter);
+
+const listen = app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);    
+});
+
+
+// module.exports = app;
+// module.exports.port = listen.address().port;
+
+export default {
+    app, 
+    port : listen.address().port
+};
